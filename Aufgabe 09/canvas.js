b39aa08d63fec3d1e6e737301f068287b545a37e
Silvia <Silvia@Silvia-MSI.fritz.box>
2019-01-01 14:52:43 +0100

    ...

--- Aufgabe 09/canvas.js
+++ Aufgabe 09/canvas.js
@@ -5,6 +5,7 @@ var Aufgabe09;
     function init() {
         canvas = document.getElementById("canvas");
         createSlope();
+        createCloud(30, 70, 30);
     }
     function createSlope() {
         var slope = canvas.getContext("2d");
@@ -23,7 +24,16 @@ var Aufgabe09;
     function createCloud(pos_x, pos_y, size) {
         var cloud = canvas.getContext("2d");
         cloud.beginPath();
-        cloud.moveTo();
+        cloud.arc(pos_x, pos_y, size, 0, 2 * Math.PI);
+        cloud.fillStyle = "white";
+        cloud.fill();
+        cloud.arc(pos_x + 70, pos_x + 20, size + 20, 0, 2 * Math.PI);
+        cloud.fillStyle = "white";
+        cloud.fill();
+        cloud.arc(pos_x + 120, pos_y - 10, size + 10, 0, 2 * Math.PI);
+        cloud.fillStyle = "white";
+        cloud.fill();
+        cloud.fillRect(30, 70, 120, size);
     }
 })(Aufgabe09 || (Aufgabe09 = {}));
 //# sourceMappingURL=canvas.js.map
--- Aufgabe 09/canvas.ts
+++ Aufgabe 09/canvas.ts
@@ -7,6 +7,7 @@ namespace Aufgabe09 {
     function init(): void {
         canvas = <HTMLCanvasElement>document.getElementById("canvas");
         createSlope();
+        createCloud(30, 70, 30);
     }
 
     function createSlope(): void {
@@ -28,6 +29,18 @@ namespace Aufgabe09 {
     function createCloud(pos_x: number, pos_y: number, size: number): void {
         var cloud = canvas.getContext("2d");
         cloud.beginPath();
-        cloud.moveTo(
+        cloud.arc(pos_x, pos_y, size, 0, 2 * Math.PI);
+        cloud.fillStyle = "white";
+        cloud.fill();
+
+        cloud.arc(pos_x + 70, pos_x + 20, size + 20, 0, 2 * Math.PI);
+        cloud.fillStyle = "white";
+        cloud.fill();
+
+        cloud.arc(pos_x + 120, pos_y - 10, size + 10, 0, 2 * Math.PI);
+        cloud.fillStyle = "white";
+        cloud.fill();
+
+        cloud.fillRect(30, 70, 120, size)
     }
 }
